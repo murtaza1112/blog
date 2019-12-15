32ab49deae5bf1125a9587d433dcf4a57f7b36a8
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2019-12-16 00:33:02 +0530

    Added scaffodling-review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_225100) do
+ActiveRecord::Schema.define(version: 2019_12_15_183535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
